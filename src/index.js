8595b07913b63b6a801a617be6464e8d9f649fbc
Ashana700 <73642738+Ashana700@users.noreply.github.com>
2021-07-11 22:03:37 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'paper';
+import {Paper } from '@material-ui/core';
 import App from './App';
 import { ContextProvider } from './SocketContext';
 import './styles.css';
